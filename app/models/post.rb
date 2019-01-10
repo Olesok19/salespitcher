750712fa4f8b85dc6ed96c707fd6a6ed811e53e8
leksus1966 <alexsokalo@gmail.com>
2019-01-10 19:39:16 +0200

    setup CarrierWave

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,6 +2,7 @@ class Post < ApplicationRecord
   has_many :reviews
   has_many :users
   self.per_page = 10
+  mount_uploader :file, FileUploader
 
   validates :title, presence: true
   validates :text, presence: true
